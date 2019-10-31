7784aabdb8ff75381f16f0322dcb7872b018e591
Manav Oza <manavoza99@gmail.com>
2019-10-31 14:58:24 +0530

    Handling Errors and Improving the Project Setup

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 // =============== Requiring Express ====================
 const express = require('express');
+const morgan = require('morgan');
 const productROutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 // =================== Executing express like a function =====================
 const app = express();
 
+// ====================== Usage of Morgan ==============================
+app.use(morgan('dev'));
+
 // =================== Handling a request ================================
 app.use('/products',productROutes);
 app.use('/orders',orderRoutes);
 
-app.use('/',(req,res,next)=>{
-    res.send('Welcome to RESTful APIs Project || You have written something wrong in URL');
+app.use((req,res,next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
 });
 
 
